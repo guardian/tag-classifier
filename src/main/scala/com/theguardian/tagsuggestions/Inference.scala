0931562a38335be83a5aaadffa220162c2234d4b
Robert Berry <robert.berry@theguardian.com>
2014-04-15 22:07:47 +0100

    Use naive Bayes classifier with Laplacian correction as inference algorithm

--- src/main/scala/com/theguardian/tagsuggestions/Inference.scala
+++ src/main/scala/com/theguardian/tagsuggestions/Inference.scala
@@ -1,33 +1,47 @@
 package com.theguardian.tagsuggestions
 
-import util.Maps.merge
-import scalaz.std.AllInstances.doubleInstance
-
-case class Triple(word1: String, word2: String, word3: String)
-
 case class Tag(tag: String)
 
-case class TripleStats(
-  triple: Triple,
-  occurrences: Int,
-  tagOccurrences: Seq[(Tag, Int)]
-)
+case class TagStats(
+  tag: Tag,
+  totalArticles: Int,
+  totalWords: Int,
+  occurrencesOfWord: Map[String, Int]
+) {
+  /** Uses Laplacian correction, so that words that do not occur do not unduly distort the model */
+  def probabilityOfWord(word: String, numberOfUniqueWordsInD: Int) = {
+    val cw = occurrencesOfWord.getOrElse(word, 0)
+    val v = numberOfUniqueWordsInD
+
+    (1 + cw).toDouble / (totalWords + v)
+  }
+
+  def probabilityOfDocument(features: Seq[String], totalNumberOfArticles: Int) = {
+    val numberUniqueFeatures = features.toSet.size
+    val probabilityOfTag = totalNumberOfArticles.toDouble / totalArticles
+
+    /** Use logs to prevent underflow issues */
+    (Seq(probabilityOfTag) ++ features.map(probabilityOfWord(_, numberUniqueFeatures))).map(Math.log).sum
+  }
+}
 
 object Inference {
-  /** Given a list of stats sequences for the triples in a piece of text, returns a sorted list of tags, and the
-    * prediction score
+  /**
+    * Given the features of a document, and a set of classes against which to classify it, returns a descending list
+    * of most likely tags, with their scores.
     *
-    * @param statsSeq The statistics
-    * @return The sorted list of tags with prediction scores
+    * @param features The features of the document (i.e., a lemmatized set of words, with stop words removed)
+    * @param classes The classes against which to match (i.e., tags, with the appropriate data for calculating
+   *                 probabilities of seeing given words in documents that have been tagged.
+    * @param totalNumberOfArticles Total number of articles the data set has been trained on.
+    * @return List of predicted tags in descending order of prediction score.
     */
   def suggestions(
-    statsSeq: Seq[TripleStats]
+    features: Seq[String],
+    classes: Set[TagStats],
+    totalNumberOfArticles: Int
   ): Seq[(Tag, Double)] =
-    statsSeq map { case TripleStats(triple, totalOccurrences, tagOccurrences) =>
-      tagOccurrences map { case (tag, occurrences) => tag -> occurrences.toDouble / totalOccurrences }
-    } reduce { (occurrences1, occurrences2) =>
-      merge(occurrences1.toMap, occurrences2.toMap).toSeq
-    } map {
-      case (tag, pSum) => tag -> pSum / statsSeq.length
-    } sortBy { -_._2 }
+    (classes map { tagStats =>
+      tagStats.tag -> tagStats.probabilityOfDocument(features, totalNumberOfArticles)
+    }).toSeq.sortBy(-_._2)
 }
