d6fe4260efd9d354db75cbe1f8fc033f78ddf007
Robert Berry <robert.berry@theguardian.com>
2014-04-24 22:19:02 +0100

    Add specs for training set builder

--- miner/src/main/scala/com/theguardian/tagclassifier/TrainingSetBuilder.scala
+++ miner/src/main/scala/com/theguardian/tagclassifier/TrainingSetBuilder.scala
@@ -25,26 +25,31 @@ object TrainingSetBuilder {
   /** Minimum number of times a feature occurs in total for it to be worth considering */
   val MinimumOccurrences = 3
 
-  def wordCounts(items: ParSeq[Document]) = items.map(_.features.frequencies).reduce(unionWith(_, _)(_ + _))
+  val ClassificationColumnName = "in class"
+
+  def wordCounts(items: ParSeq[Document]) =
+    items.map(_.features.frequencies).reduce(unionWith(_, _)(_ + _))
+
+  def buildDataSet(documents: ParSeq[Document]) = {
+    val wordFrequencies = wordCounts(documents)
+
+    val features = wordFrequencies.toSeq.filter(_._2 >= MinimumOccurrences).map(_._1).sorted
+
+    val rows = documents map { document =>
+      val documentFreqs = document.features.frequencies
+      Seq(features.map(documentFreqs.getOrElse(_, 0)) ++ List(if (document.isInClass) 1 else 0))
+    } reduce { _ ++ _ }
+
+    DataSet(features.toList ++ List(ClassificationColumnName), rows)
+  }
 
   def build(tagId: String, dataSetSize: Int) = {
     val withTag = TrainingSetDownloader.containingTag(tagId, dataSetSize / 2).map(Document.fromContent(true))
     val withoutTag = TrainingSetDownloader.notContainingTag(tagId, dataSetSize).map(Document.fromContent(false))
 
-    val documentSet = (withTag ++ withoutTag).take(dataSetSize)
+    buildDataSet((withTag ++ withoutTag).take(dataSetSize)
       .toBlockingObservable
       .toList
-      .par
-
-    val wordFrequencies = wordCounts(documentSet)
-
-    val features = wordFrequencies.toSeq.filter(_._2 >= MinimumOccurrences).map(_._1).sorted
-
-    val rows = documentSet map { document =>
-      val documentFreqs = document.features.frequencies
-      Seq(features.map(documentFreqs.getOrElse(_, 0)) ++ List(if (document.isInClass) 1 else 0))
-    } reduce { _ ++ _ }
-
-    DataSet(features.toList ++ List("inClass"), rows)
+      .par)
   }
 }
---
+++ miner/src/test/scala/com/theguardian/tagclassifier/TrainingSetBuilderSpec.scala
@@ -0,0 +1,79 @@
+package com.theguardian.tagclassifier
+
+import org.specs2.mutable.Specification
+
+class TrainingSetBuilderSpec extends Specification {
+  "wordCounts" should {
+    "correctly calculate frequencies of words" in {
+      TrainingSetBuilder.wordCounts(Seq(
+        Document(List("hello", "world"), false),
+        Document(List("hello", "is", "it", "me", "you're", "looking", "for"), false),
+        Document(List("world", "weary"), true)
+      ).par) mustEqual Map(
+        "for" -> 1,
+        "hello" -> 2,
+        "is" -> 1,
+        "it" -> 1,
+        "me" -> 1,
+        "looking" -> 1,
+        "weary" -> 1,
+        "world" -> 2,
+        "you're" -> 1
+      )
+    }
+  }
+
+  "buildDataSet" should {
+    val fixture = TrainingSetBuilder.buildDataSet(Seq(
+      Document(List("it's", "a", "whole", "new", "world"), false),
+      Document(List("it's", "show", "down", "time"), false),
+      Document(List("time", "and", "time", "again"), false),
+      Document(List("it's", "a", "damn", "shame"), true),
+      Document(List("frankly", "my", "dear", "I", "don't", "give", "a", "damn"), true)
+    ).par)
+
+    val classificationIndex = fixture.columns.length - 1
+
+    "discard words that occur fewer than 3 times" in {
+      (fixture.columns must contain("a")) and
+        (fixture.columns must contain("it's")) and
+        (fixture.columns must not contain "damn") and
+        (fixture.columns must not contain "show")
+    }
+
+    "list feature columns in alphabetical order" in {
+      fixture.columns.init.sorted mustEqual fixture.columns.init
+    }
+
+    "append a column for classifying whether the document belongs to the positive set" in {
+      fixture.columns.lastOption mustEqual Some(TrainingSetBuilder.ClassificationColumnName)
+    }
+
+    "have 0 in the final column for documents belonging to the negative set" in {
+      forall(fixture.rows.take(3).map(_.lift(classificationIndex))) { _ mustEqual Some(0) }
+    }
+
+    "have 1 in the final column for documents belonging to the positive set" in {
+      forall(fixture.rows.drop(3).map(_.lift(classificationIndex))) { _ mustEqual Some(1) }
+    }
+
+    "correctly tally frequencies across the documents" in {
+      def lookUp(column: String, row: Seq[Int]) =
+        fixture.columns.indexOf(column) match {
+          case -1 => None
+          case idx => row.lift(idx)
+        }
+
+      val firstDoc = fixture.rows.head
+
+      lookUp("it's", firstDoc) mustEqual Some(1)
+      lookUp("a", firstDoc) mustEqual Some(1)
+      lookUp("whole", firstDoc) mustEqual None
+      lookUp("time", firstDoc) mustEqual Some(0)
+
+      val thirdDoc = fixture.rows(2)
+
+      lookUp("time", thirdDoc) mustEqual Some(2)
+    }
+  }
+}
